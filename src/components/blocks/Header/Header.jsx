6ab7b2c6219f3a37cfcc6b8a066c9937b7402f17
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-03 23:19:58 +0000

    Created the Basic Header Component

--- src/components/blocks/Header/Header.jsx
+++ src/components/blocks/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import './Header.scss';
+import MenuItem from 'components/blocks/MenuItem';
+import Avatar from 'components/blocks/Avatar';
+import styles from  './Header.module.scss';
 
 const Header = ({ logo }) => {
+  const { appHeader, appLogo, menu } = styles;
   return (
-    <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
+    <header className={appHeader}>
+      <img src={logo} className={appLogo} alt="logo" />
+      <nav className={menu}>
+        <MenuItem label="Book" to="/new-appointments" />
+        <MenuItem label="Appointments" to="/" />
+        <MenuItem label="Family Members" to="/family-member" />
+        <Avatar />
+      </nav>
     </header>
   )
 }
--- src/components/blocks/Header/Header.spec.js
+++ src/components/blocks/Header/Header.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import { mount } from 'enzyme';
 import Header from './Header.jsx';
 
 describe('<Header />', () => {
-  const header = mount(<Header />);
+  const header = mount(<Router><Header /></Router>);
 
   it('should render Header as expected', () => {
     expect(header).toMatchSnapshot();
