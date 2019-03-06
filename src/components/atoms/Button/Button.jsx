7e4229e07529e3e612117a9f1a1a4e05d61606c6
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-06 01:30:08 +0000

    Add Button PropTypes

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Button.module.scss'
 
-const Button = ({ children, ...props }) => {
+const Button = ({ children, className, ...props }) => {
   const { button } = styles
   return (
-    <button className={button} {...props}>
+    <button className={`${button} ${className}`} {...props}>
       {children}
     </button>
   )
@@ -13,6 +13,7 @@ const Button = ({ children, ...props }) => {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string.isRequired,
 }
 
 export default Button
