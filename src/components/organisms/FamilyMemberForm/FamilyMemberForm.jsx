921f3a76cdab82ae11347615ad1ebf384a16cc0a
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-06 01:31:52 +0000

    Update Family Member Form

--- src/components/organisms/FamilyMemberForm/FamilyMemberForm.jsx
+++ src/components/organisms/FamilyMemberForm/FamilyMemberForm.jsx
@@ -1,19 +1,40 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Container, Row, Col } from 'react-grid-system'
+
 import TextBox from 'components/atoms/TextBox'
 import Button from 'components/atoms/Button'
+import Select from 'components/atoms/Select'
+
+import { days, years, months } from 'utils/dates'
+
+import styles from './FamilyMemberForm.module.scss'
 
 const FamilyMemberForm = ({ onSave }) => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+
   const member = {
     firstName,
     lastName,
   }
+
+  const { familyMembers, row } = styles
+
   return (
-    <Container>
-      <Row>
+    <Container className={familyMembers}>
+      <Row className={row}>
+        <Col>
+          <h2>Add Family Member</h2>
+          <p>
+            Please remember that for children under the age of 16 all
+            consultations must be via video and a parent or guardian must be
+            present.
+          </p>
+        </Col>
+      </Row>
+      <Row className={row}>
         <Col>
           <TextBox
             label="First Name"
@@ -28,6 +49,49 @@ const FamilyMemberForm = ({ onSave }) => {
             name="lastName"
             value={lastName}
             onChange={e => setLastName(e.target.value)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label htmlFor="date-of-birth">Date of Birth</label>
+        </Col>
+      </Row>
+      <Row className={row} id="date-of-birth">
+        <Col>
+          <Select className="select" placeholder="Day" options={days()} />
+        </Col>
+        <Col>
+          <Select className="select" placeholder="Month" options={months()} />
+        </Col>
+        <Col>
+          <Select className="select" placeholder="Year" options={years(1970)} />
+        </Col>
+      </Row>
+      <Row className={row}>
+        <Col>
+          <TextBox
+            label="Email"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label htmlFor="gender">Gender</label>
+        </Col>
+      </Row>
+      <Row className={row}>
+        <Col>
+          <Select
+            id="gender"
+            className="select"
+            options={[
+              { value: 'manle', label: 'Male' },
+              { value: 'female', label: 'Female' },
+            ]}
           />
         </Col>
       </Row>
