1768af21a6b6a191fa699071d145874a0d3c9951
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-07 08:49:03 +0000

    Update Avatar feature

--- src/components/atoms/Avatar/Avatar.jsx
+++ src/components/atoms/Avatar/Avatar.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Avatar.module.scss'
 
-const Avatar = ({ img, name, icon, className }) => {
-  const { avatar } = styles
-
+const Avatar = ({ img, name, icon, size, className }) => {
+  const { avatar, small, medium } = styles
+  const getSize = (size) => {
+    if (size === 'small') {
+      return small;
+    } else {
+     return medium;
+    }
+  }
   return (
-    <div className={`${avatar} ${className}`}>
+    <div className={`${avatar} ${className} ${getSize(size)}`}>
       {icon ? icon : <img src={img} alt={name} />}
     </div>
   )
 }
 
-Avatar.propTypes = {}
+Avatar.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
+  size: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired
+}
 
 export default Avatar
