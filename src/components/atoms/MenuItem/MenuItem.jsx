6d53f6a7aa82e6c47f979dd82ae2805f634b29de
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-07 08:49:23 +0000

    Add MenuItem Styles

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -5,10 +5,10 @@ import Icon from 'components/atoms/Icon'
 import styles from './MenuItem.module.scss'
 
 const MenuItem = ({ icon, label, to }) => {
-  const { menuItem } = styles
+  const { menuItem, iconItem } = styles
   return (
     <Link className={menuItem} to={to}>
-      <Icon />
+      <Icon className={iconItem}/>
       <span>{label}</span>
     </Link>
   )
