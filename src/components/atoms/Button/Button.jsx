0312476f68e676a57937b8b4d9b1e856c5276884
Leonardo Vitale <leonardo.vitale@sapient.com>
2019-03-08 16:06:10 +0000

    Modify Button behavior

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { button } from './Button.module.scss'
+import { button, link } from './Button.module.scss'
 
-const Button = ({ children, className, ...props }) => {
+const Button = ({ children, className, type, ...props }) => {
+  const style = type === 'link' ? link : button
   return (
-    <button className={`${button} ${className}`} {...props}>
+    <button className={`${style} ${className}`} {...props}>
       {children}
     </button>
   )
@@ -13,6 +14,7 @@ const Button = ({ children, className, ...props }) => {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 }
 
 export default Button
